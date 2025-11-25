44d2c0f3853a05ded78f98983e83b8a88788999f
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-11-25 22:46:50 +0200

    [!/] Duration record now 8 bytes, not 5. But has faster field access.
    It means if you're storing array with durations in fixed memory pool,
    size of array must shrink.
    
    If you're processing durations, code size will decrease and
    code speed will increase.
    
    This is example of speed/memory trade. Here we're trading memory.

--- src/me_Duration.h
+++ src/me_Duration.h
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2025-11-21
+  Last mod.: 2025-11-25
 */
 
 #pragma once
@@ -14,16 +14,24 @@ namespace me_Duration
   /*
     Duration record
 
-    Size: 40 bits (5 bytes)
+    Size: 8 bytes
     Capacity: 1 mega-second (~11 days)
     Granularity: 1 micro-second
+
+    Original structure used 10-bit fields and occupied 5 bytes.
+
+    It worked fine and you can revert back to it.
+
+    But there are more CPU operations to get/set field in this case.
+    It means minimum time to access field is higher when you're using
+    bitfields.
   */
   struct TDuration
   {
-    TUint_2 KiloS : 10;
-    TUint_2 S : 10;
-    TUint_2 MilliS : 10;
-    TUint_2 MicroS : 10;
+    TUint_2 KiloS;
+    TUint_2 S;
+    TUint_2 MilliS;
+    TUint_2 MicroS;
   };
 
   // Zero constant
@@ -67,4 +75,5 @@ namespace me_Duration
   2025-09-15
   2025-09-20 Renamed from Timestamp to Duration
   2025-10-30 Duration to micros conversion
+  2025-11-25 No bitfields in record
 */
